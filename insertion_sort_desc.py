9ec361164be7edb3f05edddc5c070e259b3605d8
vithalsamp <vithal.sampagar@gmail.com>
2019-01-28 10:38:33 +0530

    Create insertion_sort_desc.py

---
+++ insertion_sort_desc.py
@@ -0,0 +1,17 @@
+# Insertion sort Algorithm in Python
+def insertion_sort(arr):
+    for i in range(1,len(arr)):
+        # previous value from array list
+        key = arr[i]
+
+        # Next value form array list
+        j = i - 1
+        while j >= 0 and arr[j] < key:
+            arr[j+1] = arr[j]
+            j = j-1
+        arr[j+1] = key
+        #print arr
+
+arr = [31, 41, 59, 26, 41, 58]
+insertion_sort(arr)
+print arr
