b1bb418545868c3ff7ad700b3820625896b919c4
vithalsamp <vithsappi91*>
2019-02-01 08:33:23 +0530

    Updated bubble sort as per original algorithm

--- bubble_sort.py
+++ bubble_sort.py
@@ -1,13 +1,13 @@
 # Bubble sort in Python
 def bubble_sort(arr):
-    for i in range(0, len(arr)):
-        key1 = arr[i]
-        for j in range(0, len(arr)-i-1):
-            key2 = arr[j+1]
-            if arr[j] > arr[j+1]:
-                arr[j], arr[j+1] = arr[j+1], arr[j]
+    for i in range(len(arr) - 1):
+        #print 'I value', arr[i]
+        for j in range(len(arr) - 1, 0, -1):
+            #print 'J value', arr[j]
+            if arr[j] < arr[j - 1]:
+                arr[j], arr[j - 1] = arr[j - 1], arr[j]
 
 # main function
-arr = [4,2,1,3,5,4,1]
+arr = [4,2,1,3,5,4,1,4,6,8,9,3,65,34,7,2,7657,3,7,90,3,1,0]
 bubble_sort(arr)
 print(arr)
