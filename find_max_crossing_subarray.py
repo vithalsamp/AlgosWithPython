4ebbc8597bb85169638d73b89c5511a360f9c844
vithalsamp <vithsappi91*>
2019-02-07 11:05:11 +0530

    find maximum crossing sub-array

---
+++ find_max_crossing_subarray.py
@@ -0,0 +1,25 @@
+# Program to find max crsossing subarray in given array
+def find_max_crossing_subarray(arr, low, mid, high):
+    # max sum in LEFT subarray
+    left_sum = -9999999999999999
+    sum = 0
+    for i in range(mid, 0, -1):
+        sum = sum + arr[i]
+        if sum > left_sum:
+            left_sum = sum
+            max_left = i
+
+    # max sum in RIGHT subarray
+    right_sum = -9999999999999999
+    sum = 0
+    for j in range(mid+1, len(arr)-1):
+        sum = sum + arr[j]
+        if sum > right_sum:
+            right_sum = sum
+            max_right = j
+
+    print max_left, max_right, left_sum + right_sum
+
+
+arr = [1, -2, -4, 7, -2, -3, -4, 5, 6, 7, -6, -8, 6, -7, 76, 8]
+find_max_crossing_subarray(arr, 0, len(arr)/2, len(arr))
